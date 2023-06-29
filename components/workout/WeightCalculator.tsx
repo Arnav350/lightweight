2f8d985189e6d944f42a629e91ff79c22770b365
Arnav Patel <patelarnavm+github@gmail.com>
2023-06-29 11:35:47 -0400

    added weight calculator

--- components/workout/WeightCalculator.tsx
+++ components/workout/WeightCalculator.tsx
@@ -9,6 +9,15 @@ import { COLORS } from "../../constants/theme";
 function WeightCalculator() {
   const plateLbs: number[] = [55, 45, 35, 25, 10, 5, 2.5];
   const plateKgs: number[] = [25, 20, 15, 10, 5, 2.5, 1.25];
+  const plateContainerStyles = [
+    styles.plateContainerOne,
+    styles.plateContainerTwo,
+    styles.plateContainerThree,
+    styles.plateContainerFour,
+    styles.plateContainerFive,
+    styles.plateContainerSix,
+    styles.plateContainerSeven,
+  ];
 
   const { settings, setSettings } = useContext(WorkoutContext);
 
@@ -34,33 +43,21 @@ function WeightCalculator() {
     );
   }
 
-  function handleNumberBlur() {
-    let totalWeight: number = 0;
-
-    for (let i: number = 0; i < plateWeights.length; i++) {
-      totalWeight += numberValues[i] * plateWeights[i] * 2;
-    }
-
-    setTotalValue(totalWeight + barValue * 5);
-  }
-
   function handleUnitPress() {
     if (plateWeights[0] === 55) {
       setPlateWeights(plateKgs);
-      setBarValue(Math.round(barValue / 2.2));
-      setTotalValue(Math.round(totalValue / 2.2 / 2.5) * 2.5);
+      setBarValue((prevBarValue) => Math.round(prevBarValue / 2.2));
+      setTotalValue((prevTotalValue) => Math.round(prevTotalValue / 2.2 / 2.5) * 2.5);
     } else {
       setPlateWeights(plateLbs);
-      setBarValue(Math.round(barValue * 2.2));
-      setTotalValue(Math.round((totalValue * 2.2) / 5) * 5);
+      setBarValue((prevBarValue) => Math.round(prevBarValue * 2.2));
+      setTotalValue((prevTotalValue) => Math.round((prevTotalValue * 2.2) / 5) * 5);
     }
   }
 
-  function handlePress(plateIndex: number) {
-    setPlateValues(
-      plateValues.map((plateValue, i) => {
-        return plateIndex === i ? !plateValue : plateValue;
-      })
+  function handlePlatePress(plateIndex: number) {
+    setPlateValues((prevPlateValues) =>
+      prevPlateValues.map((plateValue, i) => (i === plateIndex ? !plateValue : plateValue))
     );
   }
 
@@ -81,43 +78,79 @@ function WeightCalculator() {
             <Icon name="close" size={32} color={COLORS.primary} />
           </TouchableOpacity>
           <Text style={styles.header}>Plate Calculator</Text>
-          <TouchableOpacity activeOpacity={0.3}>
+          <TouchableOpacity activeOpacity={0.3} onPress={handleUnitPress}>
             <Text style={styles.unit}>{plateWeights[0] === 55 ? "Lbs" : "Kgs"}</Text>
           </TouchableOpacity>
         </View>
-        <View>
+        <View style={styles.mainContainer}>
           <TextInput
-            value={totalValue.toString() || ""}
+            value={totalValue ? totalValue.toString() : ""}
+            keyboardType="numeric"
+            keyboardAppearance="dark"
+            style={styles.total}
             onChangeText={(text) => setTotalValue(Number(text))}
             onBlur={handleTotalBlur}
           />
-          <View>
-            <Text>Bar</Text>
-            <Text>Plates &#40;each side&#41;</Text>
+          <View style={styles.subtitlesContainer}>
+            <Text style={styles.subtitle}>Bar</Text>
+            <Text style={styles.subtitle}>Plates &#40;each side&#41;</Text>
           </View>
-          <View>
-            <View>
+          <View style={styles.weightsContainer}>
+            <View style={styles.barContainer}>
               <Slider
                 value={barValue}
                 maximumValue={plateWeights[0] === 55 ? 13 : 6}
-                onValueChange={(value) => setBarValue(value[0])}
+                thumbStyle={styles.thumb}
+                minimumTrackStyle={styles.minTrack}
+                maximumTrackStyle={styles.maxTrack}
+                onValueChange={(value) => setBarValue(Math.round(value[0]))}
+                onSlidingComplete={handleTotalBlur}
               />
-              <Text>
+              <Text style={styles.bar}>
                 {barValue * 5} {plateWeights[0] === 55 ? "lbs" : "kgs"}
               </Text>
             </View>
-            {plateWeights.map((plateWeight: number, i: number) => (
-              <View key={i}>
-                <TouchableOpacity>
-                  <Text>{plateWeight}</Text>
-                </TouchableOpacity>
-                <TextInput
-                  value={numberValues[i].toString() || ""}
-                  onChangeText={(text) => handleChange(Number(text), i)}
-                  onBlur={handleNumberBlur}
-                />
-              </View>
-            ))}
+            <View style={styles.platesContainer}>
+              {plateWeights.map((plateWeight: number, i: number) => (
+                <View key={i} style={styles.weightContainer}>
+                  <TouchableOpacity
+                    activeOpacity={0.3}
+                    style={
+                      plateValues[i]
+                        ? [styles.plateContainer, plateContainerStyles[i], { opacity: 0.3 }]
+                        : [styles.plateContainer, plateContainerStyles[i]]
+                    }
+                    onPressIn={() => handlePlatePress(i)}
+                    onPressOut={handleTotalBlur}
+                  >
+                    <Text
+                      style={
+                        [2, 4].includes(i)
+                          ? { ...styles.weight, color: COLORS.black }
+                          : i === 6
+                          ? { ...styles.weight, fontSize: 11 }
+                          : styles.weight
+                      }
+                    >
+                      {plateWeight}
+                    </Text>
+                  </TouchableOpacity>
+                  <TextInput
+                    value={numberValues[i] ? numberValues[i].toString() : ""}
+                    keyboardType="numeric"
+                    keyboardAppearance="dark"
+                    style={styles.number}
+                    onChangeText={(text) => handleChange(Number(text), i)}
+                    onBlur={() =>
+                      setTotalValue(
+                        plateWeights.reduce((total, plateWeight, i) => total + numberValues[i] * plateWeight * 2, 0) +
+                          barValue * 5
+                      )
+                    }
+                  />
+                </View>
+              ))}
+            </View>
           </View>
         </View>
       </View>
@@ -159,6 +192,129 @@ const styles = StyleSheet.create({
     color: COLORS.primary,
     fontSize: 20,
     fontWeight: "500",
+  },
+  mainContainer: {
+    padding: 8,
+  },
+  total: {
+    marginBottom: 8,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    backgroundColor: COLORS.blackOne,
+    borderRadius: 16,
+    color: COLORS.white,
+    fontSize: 32,
+    alignSelf: "center",
+  },
+  subtitlesContainer: {
+    flexDirection: "row",
+  },
+  subtitle: {
+    marginHorizontal: 8,
+    color: COLORS.white,
+    fontSize: 16,
+  },
+  weightsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  barContainer: {
+    marginRight: 8,
+  },
+  thumb: {
+    width: 8,
+    backgroundColor: COLORS.gray,
+    borderRadius: 0,
+  },
+  minTrack: {
+    height: 6,
+    backgroundColor: COLORS.gray,
+    borderRadius: 0,
+  },
+  maxTrack: {
+    backgroundColor: COLORS.darkGray,
+    borderRadius: 0,
+  },
+  bar: {
+    width: 56,
+    backgroundColor: COLORS.blackOne,
+    borderRadius: 8,
+    color: COLORS.white,
+    fontSize: 16,
+    textAlign: "center",
+    overflow: "hidden",
+  },
+  platesContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  weightContainer: {
+    margin: 2,
+    justifyContent: "center",
+    alignItems: "center",
+    height: 32,
+    width: 32,
+  },
+  plateContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+  },
+  plateContainerOne: {
+    marginVertical: 4,
+    height: 32,
+    width: 32,
+    backgroundColor: "#f33",
+  },
+  plateContainerTwo: {
+    marginVertical: 5,
+    height: 30,
+    width: 30,
+    backgroundColor: "#33f",
+  },
+  plateContainerThree: {
+    marginVertical: 5.5,
+    height: 29,
+    width: 29,
+    backgroundColor: "#ee3",
+  },
+  plateContainerFour: {
+    marginVertical: 6,
+    height: 28,
+    width: 28,
+    backgroundColor: "#393",
+  },
+  plateContainerFive: {
+    marginVertical: 7,
+    height: 26,
+    width: 26,
+    backgroundColor: "#eee",
+  },
+  plateContainerSix: {
+    marginVertical: 7.5,
+    height: 25,
+    width: 25,
+    backgroundColor: "#999",
+  },
+  plateContainerSeven: {
+    marginVertical: 8,
+    height: 24,
+    width: 24,
+    backgroundColor: "#333",
+  },
+  weight: {
+    color: COLORS.white,
+    fontWeight: "500",
+  },
+  number: {
+    width: 32,
+    height: 20,
+    backgroundColor: COLORS.blackOne,
+    borderRadius: 8,
+    color: COLORS.white,
+    fontSize: 16,
+    textAlign: "center",
   },
 });
 
